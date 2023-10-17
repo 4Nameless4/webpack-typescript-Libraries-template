bb2ab64ab83092c4178720488bb4bbe461e9b9bb
4Nameless4 <623507166@qq.com>
2023-10-17 13:06:35 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = (env, ...d) => {
+  console.log(env);
+  console.log(d);
+
+  return {
+    entry: "./src/index.(tsx|ts|js)?",
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      library: {
+        name: "webpack-libraries",
+        type: "umd",
+      },
+    },
+    devtools: "source-map",
+    module: {
+      rules: [
+        { test: /\.(ts|tsx)$/i, use: "ts-loader", exclude: /node_modules/i },
+      ],
+      rules: [
+        {
+          test: /\.css/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+  };
+};
